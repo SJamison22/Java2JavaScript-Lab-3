91513959eb316def4804b4bbdffde12799241b19
Steve Jaminson <stevejaminson@Zipcoder28s-MacBook-Pro.local>
2016-05-19 11:02:38 -0400

    Part 2 done

--- js/script.js
+++ js/script.js
@@ -10,12 +10,10 @@ function listAllNames() {
 	var newString = "";
 	var string = Object.keys(phoneBook) + " ";
 	var arr = string.split(",");
-	console.log(arr);
 	for(var i = 0; i < arr.length; i++){
 		newString += arr[i] + "<br/>";
 	}
 	display.innerHTML = newString;
-  console.log(newString);
 };
 
 function listAllNumbers() {
@@ -25,7 +23,6 @@ function listAllNumbers() {
 		for(var num in phoneBook[key]){
 			var number = phoneBook[key][num];
 			string += number + "<br/>";
-  			console.log(number);
 		}
 	}
 	display.innerHTML = string;
@@ -42,7 +39,6 @@ function showRemove() {
   var remove = prompt("Enter name to remove");
   for(var key in phoneBook){
 		var value = key;
-		console.log(value);
 		if(value == remove){
 			delete phoneBook[value];
 		}
@@ -54,16 +50,27 @@ function showLookup() {
 	var string = "";
 	for(var key in phoneBook){
 		var value = key;
-		console.log(value);
 		if(value == lookup){
 			for(var num in phoneBook[key]){
 				var number = phoneBook[key][num];
 				string += number + "<br/>";
-				console.log(number);
 			}
 		}
 	}
 	display.innerHTML = string;
+};
+
+function reverseLookup(){
+	var lookup = prompt("Enter number to lookup");
+	for(var key in phoneBook){
+		var value = phoneBook[key];
+		for(var num in phoneBook[key]){
+			var number = phoneBook[key][num];
+				if(lookup == number){
+					display.innerHTML = key;
+			}
+		}
+	}
 };
 
 var display = document.getElementById("display");
