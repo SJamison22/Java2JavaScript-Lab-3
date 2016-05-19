83839267cdc24c5b0421915cebc08bdd95de7e39
Steve Jaminson <stevejaminson@Zipcoder28s-MacBook-Pro.local>
2016-05-19 10:55:49 -0400

    Part 1 done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,69 @@
 " use strict ";
 
+var phoneBook = new Object();
+
 function PhoneBook(){
 
 };
 
 function listAllNames() {
-  // code to list all names
+	var newString = "";
+	var string = Object.keys(phoneBook) + " ";
+	var arr = string.split(",");
+	console.log(arr);
+	for(var i = 0; i < arr.length; i++){
+		newString += arr[i] + "<br/>";
+	}
+	display.innerHTML = newString;
+  console.log(newString);
 };
 
 function listAllNumbers() {
-  // code to list all numbers
+	var string = "";
+	for(var key in phoneBook){
+		var value = phoneBook[key];
+		for(var num in phoneBook[key]){
+			var number = phoneBook[key][num];
+			string += number + "<br/>";
+  			console.log(number);
+		}
+	}
+	display.innerHTML = string;
 };
 
 function showAdd() {
   var name = prompt("Enter full name");
   var number = prompt("Enter phone number");
+  phoneBook[name] = new Object();
+  phoneBook[name][number] = [number];
 };
 
 function showRemove() {
   var remove = prompt("Enter name to remove");
+  for(var key in phoneBook){
+		var value = key;
+		console.log(value);
+		if(value == remove){
+			delete phoneBook[value];
+		}
+	}
 };
 
 function showLookup() {
-  var lookup = prompt("Enter number to lookup");
+	var lookup = prompt("Enter number to lookup");
+	var string = "";
+	for(var key in phoneBook){
+		var value = key;
+		console.log(value);
+		if(value == lookup){
+			for(var num in phoneBook[key]){
+				var number = phoneBook[key][num];
+				string += number + "<br/>";
+				console.log(number);
+			}
+		}
+	}
+	display.innerHTML = string;
 };
 
 var display = document.getElementById("display");
